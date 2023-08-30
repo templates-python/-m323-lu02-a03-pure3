dad344010eb6758ca4812f2c63587f05bc4706b9
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-08-30 10:05:51 +0200

    done

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 # your code for function average goes here
+def average(list):
+    if len(list) > 0:
+        return sum(list)/len(list)
+    else:
+        return 0
 
 if __name__ == "__main__":
     list = [10, 20, 30, 40, 50]
